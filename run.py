b8bd5f888a0f64e2b94eb449915a412f5c0fca32
donchris90 <donchris4life2006@gmail.com>
2025-08-20 00:55:43 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from app import create_app, socketio
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    import eventlet
-    import eventlet.wsgi
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port if available
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
