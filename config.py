2a6094e9699f62e220c3cc3057106101316a0adf
donchris90 <donchris4life2006@gmail.com>
2025-08-20 11:16:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,19 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+    # Ensure SQLAlchemy uses psycopg3
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
         "pool_recycle": 300
     }
+
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'uploads')
     INSPECTION_UPLOAD_FOLDER = 'app/static/uploads/inspection_files'
 
@@ -18,7 +25,3 @@ class Config:
 
     PAYSTACK_SECRET_KEY = os.getenv('PAYSTACK_SECRET_KEY')
     PAYSTACK_PUBLIC_KEY = os.getenv('PAYSTACK_PUBLIC_KEY')
-
-
-
-
